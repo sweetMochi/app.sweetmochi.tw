beb710dbf6798e0fdcdc57b40772c0937eda9afe
sweetmochi <thepaperempty@gmail.com>
2024-10-16 18:24:36 +0800

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,53 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { urlValidators, youTubeValidators } from '../base/validator/url';
+
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.less'
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.less'
 })
-export class AppComponent {
-  title = 'app.sweetmochi.tw';
+export class AppComponent implements OnInit {
+
+
+	/** 圖片路徑 */
+	imgSrc = '';
+
+	/** 驗證表單 */
+	formGroup = this.formBuilder.group(
+		{
+			url: ['', [Validators.required, urlValidators(), youTubeValidators()]]
+		},
+		{
+			updateOn: 'submit'
+		}
+	);
+
+	constructor(private formBuilder: FormBuilder) {}
+
+
+	ngOnInit(): void {
+
+	}
+
+
+	/**
+	 * 送出表單事件
+	 */
+	onSubmit(): void {
+
+		if (this.formGroup.invalid) {
+			return;
+		}
+
+		let url = new URL(this.formGroup.value.url || '');
+
+		let id = url.searchParams.get('v');
+
+		this.imgSrc = `https://img.youtube.com/vi/${id}/maxresdefault.jpg`;
+
+	}
+
+
 }
---
+++ src/app/app.module.ts
@@ -0,0 +1,51 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		ReactiveFormsModule,
+		MatIconModule,
+		MatButtonModule,
+		MatToolbarModule,
+		MatSidenavModule,
+		MatButtonModule,
+		MatListModule,
+		MatInputModule,
+		MatCardModule,
+		MatFormFieldModule
+	],
+	exports: [
+		MatIconModule,
+		MatButtonModule,
+		MatToolbarModule,
+		MatSidenavModule,
+		MatButtonModule,
+		MatListModule,
+		MatInputModule,
+		MatCardModule,
+		MatFormFieldModule
+	],
+	providers: [
+		provideAnimationsAsync()
+	],
+	bootstrap: [AppComponent]
+})
+export class AppModule { }
